73a3181b27037afb6398061852258cd9fc6b0866
shorouqpedia <shorouq96@yahoo.com>
2019-07-27 19:29:01 +0200

    naming automaticly the maintainance

--- fix.php
+++ fix.php
@@ -4,44 +4,37 @@ session_start();
 $title = "maintenance";
 $active = "clients";
 require_once 'partials/init.php';
-if ($_SERVER['REQUEST_METHOD'] === 'POST') 
+//if ($_SERVER['REQUEST_METHOD'] === 'POST') 
 {
-    $id = filter_var(intval($_POST['id']), FILTER_SANITIZE_NUMBER_INT); 
-    $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);        
-    $query = $con->prepare("INSERT INTO `maintenance_history` (`cid`,`name`) VALUES (?,?)");
-    $query->execute(array($id,$name));
-    if ($query->rowCount() > 0) 
+    
+    $id=$_GET['id'];
+    $q=$con->prepare("SELECT * from clients WHERE id=$id");
+    $q->execute(array($id));
+    if ($q->rowCount() > 0) 
     {
-        ?><h3>Successfull</h3><?php                
-        header('Location:maintain.php?cid='.$id);
-        exit();
+        $client = $q->fetchAll(PDO::FETCH_ASSOC)[0];
+        $KM_counter=$client['KM_counter'];
+    }
+    $name = $KM_counter;
+    $check=mysqli_query($con,"SELECT * from maintenance_history WHERE name='$name'");
+    $checkrows=mysqli_num_rows($check);
+    if($checkrows>0) 
+    { 
+        $query = $con->prepare("INSERT INTO `maintenance_history` (`cid`,`name`) VALUES ($id,$name)");
+        $query->execute(array($id,$name));
+        if ($query->rowCount() > 0)
+        {
+            ?><h3>Successfull</h3><?php                
+            header('Location:maintain.php?cid='.$id);
+            exit();
+        }
+    }
+    else
+    {
+        echo "لقد تمت الصيانه لهذا العداد برجاء مراجعة عداد الكيلو";
     }
 }
-else { ?>
-    <div class="container pt-3 reg-form align-items-center">
-        
-        <form class="col-12 col-sm-10 col-md-8 col-xl-6" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post"
-              id="registerForm" name="registerForm" enctype="multipart/form-data">
-
-            <?php 
-            $id=$_GET['id'];
-            ?>
-            <div class="form-group">
-                <label for='name' class="control-label">
-                    Name
-                </label>
-                <input data-check="[^A-Za-z ]" id=name name=name type="text" class="form-control" placeholder="maintenance name">
-            </div>
-            <input hidden="TRUE" type="number" name="id" value=<?php echo $id ?> >
-            <?php 
-            }?>
-
-            <div class="form-group mb-5">
-                <input type="submit" class="form-control btn btn-success" value="Submit">
-            </div>
-
-        </form>
-    </div>
+?>
 
     <script src="validation.js"></script>
 	<script>
