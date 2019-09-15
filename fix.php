73e1066e6e06e39625daf1f7e958af1feddc07d4
shorouqpedia <shorouq96@yahoo.com>
2019-09-15 19:35:32 +0200

    add kilo meters while maintaining

--- fix.php
+++ fix.php
@@ -4,37 +4,41 @@ session_start();
 $title = "maintenance";
 $active = "clients";
 require_once 'partials/init.php';
-//if ($_SERVER['REQUEST_METHOD'] === 'POST') 
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') 
 {
-    
-    $id=$_GET['id'];
-    $q=$con->prepare("SELECT * from clients WHERE id=$id");
-    $q->execute(array($id));
-    if ($q->rowCount() > 0) 
+    $name=$_POST['km'];
+    $id=$_POST['id'];
+    $query = $con->prepare("INSERT INTO `maintenance_history` (`cid`,`name`) VALUES ($id,$name)");
+    $query->execute(array($id,$name));
+    if ($query->rowCount() > 0)
     {
-        $client = $q->fetchAll(PDO::FETCH_ASSOC)[0];
-        $KM_counter=$client['KM_counter'];
-    }
-    $name =$KM_counter;
-    //$check=mysqli_query($con,"SELECT * from maintenance_history WHERE name='$name'");
-    //$checkrows=mysqli_num_rows($check);
-    if(TRUE)//($checkrows>0) 
-    { 
-        $query = $con->prepare("INSERT INTO `maintenance_history` (`cid`,`name`) VALUES ($id,$name)");
-        $query->execute(array($id,$name));
-        if ($query->rowCount() > 0)
-        {
-            ?><h3>Successfull</h3><?php                
-            header('Location:maintain.php?cid='.$id);
-            exit();
-        }
-    }
-    else
-    {
-        echo "لقد تمت الصيانه لهذا العداد برجاء مراجعة عداد الكيلو";
+        ?><h3>Successfull</h3><?php                
+        header('Location:maintain.php?cid='.$id);
+        exit();
     }
 }
+else
+{   
+    $id=$_GET['id'];
 ?>
+    <div class="container pt-3 reg-form">
+        <form class="col-12 col-sm-10 col-md-8 col-xl-6" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post"
+              id="maintain" name="maintain" enctype="multipart/form-data">
+            <div class="form-group">
+                <label for="km" class="control-label"> current KM counter </label>
+                <input id='km' name='km' type="number" class="form-control" placeholder='eg: 1000' ">
+            </div>
+            <input hidden="TRUE" type="number" name="id" value=<?php echo $id ?> >
+            <?php 
+            ?>
+            <div class="form-group mb-5">
+                <input type="submit" class="form-control btn btn-success" value='تم'>
+            </div>
+        </form>
+    </div>
+<?php }?> 
 
     <script src="validation.js"></script>
 	<script>
