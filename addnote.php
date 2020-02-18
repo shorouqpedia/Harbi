80ba1d5160e4bfad5977301cabf1bc8b3989d16f
shorouqpedia <shorouq96@yahoo.com>
2020-02-18 17:34:33 +0200

    Update addnote.php

--- addnote.php
+++ addnote.php
@@ -22,8 +22,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
         $note
         ));
 
-        header("location:" . $_SERVER['PHP_SELF'] . "?id=" . $id);
-       
+	$redirect_url = str_replace('addnote', 'clients', $_SERVER['PHP_SELF']) . "?id=" . $id; 
+        header("location:" . $redirect_url);        
     }
 }
 else { ?>
